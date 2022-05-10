27ac97bb77b9abafe185389e2537c19849c619fe
byeong <abm1640@naver.com>
2022-05-10 11:14:47 +0900

    인덱스 스크립트, 영상 수정, 서비스 로고 변경

--- js/index-swipe.js
+++ js/index-swipe.js
@@ -1,64 +1,41 @@
-jQuery(document).ready(function(){
+// jQuery(document).ready(function(){
 
-    var a = 1;
-    // $("#index-left-btn").click(function(e){
-    //     e.preventDefault();
+//     var a = 1;
 
+//     $("#index-left-btn").click(function(e){
 
-    //     $("#index-video video").eq(0).stop().animate({
-    //         marginLeft : "-100"*a+"%"
-    //     },1000);
+//         e.preventDefault();
 
-    //     a++;
+//         $("#index-video video").eq(0).animate({
+//             marginLeft : a*"-100"+"%"
+//         },1000);
 
-    //     console.log(a);
-    //     if(a>=video.length)(a=0);
-    // });
+//         a++;
 
-
-    // $("#index-right-btn").click(function(e){
-    //     e.preventDefault();
-
-
-    //     $("#index-video video").eq(0).stop().animate({
-    //         marginLeft : "100"*a+"%"
-    //     },1000);
-
-    //     a--;
-
-    //     console.log(a);
-    //     if(a<=-4)(a=0)
-    //     else if(a == 0)(a = 1);
-    // });
-
-    $("#index-left-btn").click(function(e){
-
-        e.preventDefault();
-
-        $("#index-video video").eq(0).animate({
-            marginLeft : a*"-100"+"%"
-        },1000);
-
-        a++;
-
-        if(a == 4)(a = 0);
-        console.log(a);
-    });
+//         if(a == 4)(a = 0);
+//         console.log(a);
+//     });
 
 
 
-    $("#index-right-btn").click(function(e){
+//     $("#index-right-btn").click(function(e){
 
-        e.preventDefault();
+//         e.preventDefault();
 
-        $("#index-video video").eq(0).animate({
-            marginLeft : a*"100"+"%"
-        },1000);
+//         $("#index-video video").eq(0).animate({
+//             marginLeft : a*"100"+"%"
+//         },1000);
         
-        a--;
+//         a--;
 
-        if(a == -4)(a = 0)
-        console.log(a);
-    });
-});
+//         if(a == -4)(a = 0)
+//         console.log(a);
+//     });
+// });
 
+var swiper = new Swiper(".mySwiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
